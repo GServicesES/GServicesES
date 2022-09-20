b332ac9f23f2c5725de54f6e1b33874bb1dd31e7
Hester910 <hadassa.ester@ufrpe.br>
2022-09-20 14:53:52 -0300

    Rotas e schema de cliente

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_20_020855) do
+  create_table "clientes", force: :cascade do |t|
+    t.string "nome_completo"
+    t.date "data_de_nascimento"
+    t.string "email"
+    t.string "telefone"
+    t.string "cpf"
+    t.string "senha"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "enderecos", force: :cascade do |t|
+    t.string "cep"
+    t.string "cidade"
+    t.string "bairro"
+    t.string "logradouro"
+    t.string "complemento"
+    t.integer "administrador_id", null: false
+    t.integer "cliente_id", null: false
+    t.integer "trabalhador_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["administrador_id"], name: "index_enderecos_on_administrador_id"
+    t.index ["cliente_id"], name: "index_enderecos_on_cliente_id"
+    t.index ["trabalhador_id"], name: "index_enderecos_on_trabalhador_id"
+  end
+
+  add_foreign_key "enderecos", "administradors"
+  add_foreign_key "enderecos", "clientes"
+  add_foreign_key "enderecos", "trabalhadors"
 end
